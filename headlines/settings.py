d6089e81ef31f959d912bf024154566b0b08af2e
SirIsaacNeutron <dbadir@cox.net>
2019-07-25 13:04:10 -0700

    Created basic homepage

--- headlines/settings.py
+++ headlines/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django_secrets',
+    'news.apps.NewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
