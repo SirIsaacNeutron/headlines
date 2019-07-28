e8b7ff8c8395aaeaa1ecbfed0f79bf242a33fefd
SirIsaacNeutron <dbadir@cox.net>
2019-07-27 22:06:03 -0700

    Fix Heroku database

--- headlines/settings.py
+++ headlines/settings.py
@@ -127,3 +127,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 import django_heroku
 django_heroku.settings(locals())
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
